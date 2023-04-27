3e6097cbf74784e845af40b224d59af42a3e0c57
rishix23 <rishi8318@gmail.com>
2023-04-27 21:57:59 +0100

    adding .env for frontend

--- server/server.js
+++ server/server.js
@@ -9,13 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 // setting port
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
-// uri for Netifly
+// uri for mongo docker
 const uri = process.env.URI
-
-// uri for docker
-//const uri = process.env.MONGO_URI;
 
 // database init
 mongoose.connect(uri);
@@ -28,15 +25,18 @@ connection.once("open", () => {
 const exercisesRouter = require("./routes/exercises");
 const blogsRouter = require("./routes/blogs");
 
-app.use("/exercises", exercisesRouter);
-app.use("/blogs", blogsRouter);
+if (process.env.NODE_ENV === 'production') {
+  app.use('/api/exercises', exercisesRouter);
+  app.use('/api/blogs', blogsRouter);
+} else {
+  app.use("/exercises", exercisesRouter);
+  app.use("/blogs", blogsRouter);
+}
 
-app.get("/code", (req, res) => {
-
-
-    //res.redirect("http://localhost:3000/about")
-  });
+// app.get("/code", (req, res) => {
+//     //res.redirect("http://localhost:3000/about")
+//   });
   
-app.listen(5000, () => {
-  console.log(`Server started on port ${5000}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
