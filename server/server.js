73b2a57f07b894e73c2277e1c2aced3d3aad32c6
rishix23 <rishi8318@gmail.com>
2023-04-28 18:56:29 +0100

    netifly debugging

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // setting port
 // const port = process.env.PORT // || 5000;
-const port = process.env.port
+const port = process.env.port || 80
 
 // uri for mongo docker
 const uri = process.env.URI
