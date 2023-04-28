6e5c2eee92119664b9e50a37344341c7be2e663b
rishix23 <rishi8318@gmail.com>
2023-04-28 23:44:22 +0100

    updating package.json

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // setting port
 // const port = process.env.PORT // || 5000;
-const port = process.env.port || 80
+const port = process.env.PORT
 
 // uri for mongo docker
 const uri = process.env.URI
