6c78f26e73458bb935a9f7c8df44d62d79bb5929
rishix23 <rishi8318@gmail.com>
2023-04-29 15:46:08 +0100

    updating server.js

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ const port = process.env.PORT
 const uri = 'mongodb+srv://rishix23:Absegami16!@cluster1.insdjk8.mongodb.net/?retryWrites=true&w=majority'
 
 // database init
-mongoose.connect(uri);
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+// mongoose.connect(uri);
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
 
 // routes
 // const blogsRouter = require("./routes/blogs");
