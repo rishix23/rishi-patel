6af344c1de6ec246b3b224ced4a57246d38c671d
rishix23 <rishi8318@gmail.com>
2023-05-10 23:40:33 +0100

    test

--- frontend/src/api/spotify.js
+++ frontend/src/api/spotify.js
@@ -6,7 +6,7 @@ import React from "react";
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 const REDIRECT_URI = process.env.NODE_ENV === "production" ? "https://rishipatel.netlify.app/about/callback" : "http://localhost:3000/about/callback";
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=user-top-read`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=user-top-read`;
 
 const Spotify = () => {
   const [accessToken, setAccessToken] = useState("");
@@ -24,65 +24,30 @@ const Spotify = () => {
     }
   };
 
-  // check if the auth code has expired
-  const isAuthCodeExpired = () => {
-    const hasBeenOneHour = new Date() - new Date(Cookies.get("authCodeDate")) >= 3600000;
-    if (hasBeenOneHour) {
-      Cookies.set("authCodeDate", "");
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  // authorises on page load
+  // get access token from URL hash if available
   useEffect(() => {
-    if ((Cookies.get("code") == null && new URLSearchParams(window.location.search).get("code") == null) || isAuthCodeExpired()) {
-      Cookies.set("authCodeDate", new Date());
+    const hashParams = window.location.hash.substr(1);
+    const hashParamsObject = Object.fromEntries(new URLSearchParams(hashParams));
+    const storedAccessToken = Cookies.get("accessToken");
+
+    if (hashParamsObject.access_token) {
+      setAccessToken(hashParamsObject.access_token);
+      Cookies.set("accessToken", hashParamsObject.access_token);
+      Cookies.set("accessTokenDate", new Date());
+      window.location.hash = "";
+    } else if (storedAccessToken) {
+      setAccessToken(storedAccessToken);
+    } else {
       window.location.href = AUTH_URL;
     }
   }, []);
-
-  // generates access token if not already stored in local storage or token expires
-  useEffect(() => {
-    var code = new URLSearchParams(window.location.search).get("code");
-    if (!code) {
-      code = Cookies.get("code");
-    }
-    const storedAccessToken = Cookies.get("accessToken");
-
-    if ((code && (storedAccessToken == null || storedAccessToken === "" || storedAccessToken === "undefined")) || (code && isTokenExpired())) {
-      Cookies.set("code", code);
-      const data = {
-        grant_type: "authorization_code",
-        code,
-        redirect_uri: REDIRECT_URI,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-      };
-
-      fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(data).toString(),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setAccessToken(data.access_token);
-          Cookies.set("accessToken", data.access_token);
-          Cookies.set("accessTokenDate", new Date());
-        });
-    } else if (storedAccessToken) {
-      setAccessToken(storedAccessToken);
-    }
-  }, [accessToken]);
 
   // gets top tracks for initial page load (short term)
   useEffect(() => {
     if (accessToken && topTracks.length === 0) {
       getTopTracks("short_term");
     }
-  });
+  }, [accessToken]);
 
   // gets top tracks from medium/long term frames
   async function getTopTracks(timeFrame) {
@@ -104,10 +69,10 @@ const Spotify = () => {
   return (
     <>
       <div className={styles.topTracksSection}>
+        <h4>Top Tracks</h4>
         <button onClick={(e) => getTopTracks("short_term", e)}>Most Recent</button>
         <button onClick={(e) => getTopTracks("medium_term", e)}>1 Year</button>
         <button onClick={(e) => getTopTracks("long_term", e)}>All time</button>
-        <h3>Top Tracks</h3>
         <div className={styles.topTracksBox}>
           {topTracks.length > 0 &&
             topTracks.map((track) => (
