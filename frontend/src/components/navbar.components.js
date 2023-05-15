5d1e71cd45a75e3c4197bb299c6294fd3d8a35f8
rishix23 <rishi8318@gmail.com>
2023-05-15 23:56:38 +0100

    updating navbar to tailwind

--- frontend/src/components/navbar.components.js
+++ frontend/src/components/navbar.components.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="navbar-brand">Rishi</Link>
-      <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <Link to="/about" className="nav-link">About</Link>
-          </li> 
-          <li className="navbar-item">
-            <Link to="/blogs" className="nav-link">Blog</Link>
-          </li> 
-          <li className="navbar-item">
-            <Link to="/projects" className="nav-link">Projects</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/photos" className="nav-link">Photos</Link>
-          </li>
-        </ul>
+    <nav className="bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <Link
+              to="/"
+              className="text-white text-lg font-semibold">
+              Rishi
+            </Link>
+          </div>
+          <div className="hidden md:block flex justify-center">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                to="/about"
+                className="text-gray-300 hover:text-white hover:bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300">
+                About
+              </Link>
+              <Link
+                to="/blogs"
+                className="text-gray-300 hover:text-white hover:bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300">
+                Blog
+              </Link>
+              <Link
+                to="/projects"
+                className="text-gray-300 hover:text-white hover:bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300">
+                Projects
+              </Link>
+              <Link
+                to="/photos"
+                className="text-gray-300 hover:text-white hover:bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300">
+                Photos
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
