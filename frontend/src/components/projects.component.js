0d258abb1211dacdd6104dc5361e777889274bb0
rishix23 <rishi8318@gmail.com>
2023-05-26 22:33:45 +0100

    adding comments

--- frontend/src/components/projects.component.js
+++ frontend/src/components/projects.component.js
@@ -1,26 +1,29 @@
-import styles from './projects.module.css';
-import { useEffect, useState } from "react"
-import { getRepos } from "../api/git"
+import styles from "./projects.module.css";
+import { useEffect, useState } from "react";
+import { getRepos } from "../api/git";
 
 const ProjectList = () => {
-  const [repos, setRepos] = useState([])
+  const [repos, setRepos] = useState([]);
 
+  // gets repositories from GitHub
   useEffect(() => {
-    getRepos().then((repos) => {
-      setRepos(repos)
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    getRepos()
+      .then((repos) => {
+        setRepos(repos);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
-  
 
   return (
     <div className={styles.projectsContainer}>
       <h2>Personal Coding Projects</h2>
       <ul>
         {repos.map((repo, index) => (
-          <li key={index} className={styles.projectItem}>
+          <li
+            key={index}
+            className={styles.projectItem}>
             <h3>{repo.name}</h3>
             <p>Date: {repo.created_at}</p>
             <p>Description: {repo.description}</p>
