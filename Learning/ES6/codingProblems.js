e81e9d1ebe9b18bf1c66f2083c4f47c3219b8cae
rishix23 <rishi8318@gmail.com>
2023-12-29 23:20:26 -0500

    solved container with most water problem :)

--- Learning/ES6/codingProblems.js
+++ Learning/ES6/codingProblems.js
@@ -448,7 +448,7 @@
 // };
 // productExceptSelf(nums);
 
-// IS PALINDROME //BETTER EFFCIENCY?
+// IS PALINDROME // BETTER EFFCIENCY?
 // var isPalindrome = function (s) {
 //   arr = s
 //     .replace(/[^a-zA-Z0-9]/g, "")
@@ -473,7 +473,7 @@
 // };
 // console.log(isPalindrome("a"));
 
-// LONGEST CONSECUTIVE O(n) 
+// LONGEST CONSECUTIVE O(n)
 // nums = [50, 5, 23, 6743, 234, 6345, -1, 4, 3, -2, 434, -4, 2, 1, 9, 800, -5];
 
 // var longestConsecutive = function (nums) {
@@ -550,101 +550,107 @@
 
 // -4 + 2 + (target - L+R) == target
 
-nums = [-2,-1,-1,0,1,2,3,4]
-function threeSum(nums) {
-	const results = []
+// THREE SUM - COMPLETTION REQUIRED
+// nums = [-2,-1,-1,0,1,2,3,4]
+// function threeSum(nums) {
+// 	const results = []
 
-	if (nums.length < 3) return results
-	nums = nums.sort((a, b) => a - b)
+// 	if (nums.length < 3) return results
+// 	nums = nums.sort((a, b) => a - b)
 
-	let target = 0
+// 	let target = 0
 
-	for (let i = 0; i < nums.length - 2; i++) {
+// 	for (let i = 0; i < nums.length - 2; i++) {
 
-		if (nums[i] > target) break
+// 		if (nums[i] > target) break
 
-		// we don't want repeats, so skip numbers we've already seen
-		if (i > 0 && nums[i] === nums[i - 1]) continue
+// 		// we don't want repeats, so skip numbers we've already seen
+// 		if (i > 0 && nums[i] === nums[i - 1]) continue
 
-		// `j` represents the "middle" element between `i` and `k`.
-		// we will increment this up through the array while `i` and `k`
-		// are anchored to their positions. we will decrement `k` for
-		// for each pass through the array, and finally increment `i`
-		// once `j` and `k` meet.
-		let j = i + 1
+// 		// `j` represents the "middle" element between `i` and `k`.
+// 		// we will increment this up through the array while `i` and `k`
+// 		// are anchored to their positions. we will decrement `k` for
+// 		// for each pass through the array, and finally increment `i`
+// 		// once `j` and `k` meet.
+// 		let j = i + 1
 
-		// `k` represents the "right" most element
-		let k = nums.length - 1
-		
-		// to summarize our setup, we have `i` that starts at the beginning,
-		// `k` that starts at the end, and `j` that races in between the two.
-		//
-		// note that `i` is controlled by our outer for-loop and will move the slowest.
-		// in the meantime, `j` and `k` will take turns inching towards each other depending
-		// on some logic we'll set up below. once they collide, `i` will be incremented up
-		// and we'll repeat the process.
+// 		// `k` represents the "right" most element
+// 		let k = nums.length - 1
 
-		while (j < k) {
-			let sum = nums[i] + nums[j] + nums[k]
+// 		// to summarize our setup, we have `i` that starts at the beginning,
+// 		// `k` that starts at the end, and `j` that races in between the two.
+// 		//
+// 		// note that `i` is controlled by our outer for-loop and will move the slowest.
+// 		// in the meantime, `j` and `k` will take turns inching towards each other depending
+// 		// on some logic we'll set up below. once they collide, `i` will be incremented up
+// 		// and we'll repeat the process.
 
-			// if we find the target sum, increment `j` and decrement `k` for
-			// other potential combos where `i` is the anchor
-			if (sum === target) {
-				// store the valid threesum
-				results.push([nums[i], nums[j], nums[k]])
+// 		while (j < k) {
+// 			let sum = nums[i] + nums[j] + nums[k]
 
-				// this is important! we need to continue to increment `j` and decrement `k`
-				// as long as those values are duplicated. in other words, we wanna skip values
-				// we've already seen. otherwise, an input array of [-2,0,0,2,2] would result in
-				// [[-2,0,2], [-2,0,2]].
-				//
-				// (i'm not a fan of this part because we're doing a while loop as we're
-				// already inside of another while loop...)
-				while (nums[j] === nums[j + 1]) j++
-				while (nums[k] === nums[k - 1]) k--
+// 			// if we find the target sum, increment `j` and decrement `k` for
+// 			// other potential combos where `i` is the anchor
+// 			if (sum === target) {
+// 				// store the valid threesum
+// 				results.push([nums[i], nums[j], nums[k]])
 
-				// finally, we need to actually move `j` forward and `k` backward to the
-				// next unique elements. the previous while loops will not handle this.
-				j++
-				k--
+// 				// this is important! we need to continue to increment `j` and decrement `k`
+// 				// as long as those values are duplicated. in other words, we wanna skip values
+// 				// we've already seen. otherwise, an input array of [-2,0,0,2,2] would result in
+// 				// [[-2,0,2], [-2,0,2]].
+// 				//
+// 				// (i'm not a fan of this part because we're doing a while loop as we're
+// 				// already inside of another while loop...)
+// 				while (nums[j] === nums[j + 1]) j++
+// 				while (nums[k] === nums[k - 1]) k--
 
-			// if the sum is too small, increment `j` to get closer to the target
-			} else if (sum < target) {
-				j++
+// 				// finally, we need to actually move `j` forward and `k` backward to the
+// 				// next unique elements. the previous while loops will not handle this.
+// 				j++
+// 				k--
 
-			// if the sum is too large, decrement `k` to get closer to the target
-			} else { // (sum > target)
-				k--
-			}
-		}
-	}
+// 			// if the sum is too small, increment `j` to get closer to the target
+// 			} else if (sum < target) {
+// 				j++
 
-	return result
-// // Creating a hash set using objects
-// const hashSet = {};
+// 			// if the sum is too large, decrement `k` to get closer to the target
+// 			} else { // (sum > target)
+// 				k--
+// 			}
+// 		}
+// 	}
+// 	return result
 
-// // Adding elements to the hash set
-// hashSet["apple"] = true;
-// hashSet["banana"] = true;
-// hashSet["orange"] = true;
-// hashSet["test"] = "hello";
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+var maxArea = function (height) {
+  //need to check if the arr has a legnth greater than else return 0 - done
+  // need to set pointers to left and right - done
+  // increment and decrement pointers based on which pointer is lower - done
+  // get the minimum between the two then calculate the distance between the left and right pointers - done
+  // set a max area variable for each 2 sets of points - done
+  // update max area var whenever a new max area is calculated - done
 
-// delete hashSet["banana"];
-// // Checking for membership
-// console.log(hashSet); // true
+  if (height.length < 2) {
+    return 0;
+  }
 
-// Creating a hash set using Maps
-// let hashSetNew = new Map();
+  L = 0;
+  R = height.length - 1;
+  maxArea = 0;
 
-// // // Adding elements to the hash set
-// hashSetNew.set(0, 2);
-// hashSetNew.set(1, 1);
-// hashSetNew.set(2, 5);
+  while (L < R) {
+    minHeight = Math.min(height[L], height[R]);
+    distanceBetween = R - L;
+    currentMaxArea = minHeight * distanceBetween;
+    maxArea = Math.max(maxArea, currentMaxArea);
+    if (height[L] < height[R]) {
+      L++;
+    } else {
+      R--;
+    }
+  }
+  return maxArea;
+  //   console.log(maxArea);
+};
 
-// hashSetNew.get("array").push(2);
-
-// console.log(hashSetNew.get("array"));
-
-// console.log(hashSetNew.get("array"));
-
-// console.log(hashSetNew.get("apple"));
+console.log(maxArea(height));
