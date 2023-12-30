f7a3d16ec62dc698eceb34f4444164a6a3bf2e9c
rishix23 <rishi8318@gmail.com>
2023-12-29 22:42:15 -0500

    updating code problems

--- Learning/ES6/codingProblems.js
+++ Learning/ES6/codingProblems.js
@@ -473,7 +473,7 @@
 // };
 // console.log(isPalindrome("a"));
 
-// LONGEST CONSECUTIVE O(n)
+// LONGEST CONSECUTIVE O(n) 
 // nums = [50, 5, 23, 6743, 234, 6345, -1, 4, 3, -2, 434, -4, 2, 1, 9, 800, -5];
 
 // var longestConsecutive = function (nums) {
@@ -499,7 +499,7 @@
 //   return max;
 // };
 
-// LONGEST CONSECUTIVE O(n log n)
+// LONGEST CONSECUTIVE O(n log n) // original solution
 // var longestConsecutive = function (nums) {
 //   set = new Set(nums);
 //   nums = Array.from(set);
@@ -527,28 +527,99 @@
 // longestConsecutive(nums);
 
 // 167. Two Sum II - Input Array Is Sorted
-nums = [2, 3, 4];
-var twoSum = function (numbers, target) {
-  l = 0;
-  r = numbers.length - 1;
-  finalArr = [];
+// nums = [2, 3, 4];
+// var twoSum = function (numbers, target) {
+//   l = 0;
+//   r = numbers.length - 1;
+//   finalArr = [];
 
-  while (l < r) {
-    if (numbers[l] + numbers[r] == target) {
-      finalArr.push(l + 1);
-      finalArr.push(r + 1);
-    }
-    if (numbers[l] + numbers[r] > target) {
-      r--;
-    } else {
-      l++;
-    }
-  }
-  return finalArr;
-};
+//   while (l < r) {
+//     if (numbers[l] + numbers[r] == target) {
+//       finalArr.push(l + 1);
+//       finalArr.push(r + 1);
+//     }
+//     if (numbers[l] + numbers[r] > target) {
+//       r--;
+//     } else {
+//       l++;
+//     }
+//   }
+//   return finalArr;
+// };
+// twoSum(nums, 6);
 
-twoSum(nums, 6);
+// -4 + 2 + (target - L+R) == target
 
+nums = [-2,-1,-1,0,1,2,3,4]
+function threeSum(nums) {
+	const results = []
+
+	if (nums.length < 3) return results
+	nums = nums.sort((a, b) => a - b)
+
+	let target = 0
+
+	for (let i = 0; i < nums.length - 2; i++) {
+
+		if (nums[i] > target) break
+
+		// we don't want repeats, so skip numbers we've already seen
+		if (i > 0 && nums[i] === nums[i - 1]) continue
+
+		// `j` represents the "middle" element between `i` and `k`.
+		// we will increment this up through the array while `i` and `k`
+		// are anchored to their positions. we will decrement `k` for
+		// for each pass through the array, and finally increment `i`
+		// once `j` and `k` meet.
+		let j = i + 1
+
+		// `k` represents the "right" most element
+		let k = nums.length - 1
+		
+		// to summarize our setup, we have `i` that starts at the beginning,
+		// `k` that starts at the end, and `j` that races in between the two.
+		//
+		// note that `i` is controlled by our outer for-loop and will move the slowest.
+		// in the meantime, `j` and `k` will take turns inching towards each other depending
+		// on some logic we'll set up below. once they collide, `i` will be incremented up
+		// and we'll repeat the process.
+
+		while (j < k) {
+			let sum = nums[i] + nums[j] + nums[k]
+
+			// if we find the target sum, increment `j` and decrement `k` for
+			// other potential combos where `i` is the anchor
+			if (sum === target) {
+				// store the valid threesum
+				results.push([nums[i], nums[j], nums[k]])
+
+				// this is important! we need to continue to increment `j` and decrement `k`
+				// as long as those values are duplicated. in other words, we wanna skip values
+				// we've already seen. otherwise, an input array of [-2,0,0,2,2] would result in
+				// [[-2,0,2], [-2,0,2]].
+				//
+				// (i'm not a fan of this part because we're doing a while loop as we're
+				// already inside of another while loop...)
+				while (nums[j] === nums[j + 1]) j++
+				while (nums[k] === nums[k - 1]) k--
+
+				// finally, we need to actually move `j` forward and `k` backward to the
+				// next unique elements. the previous while loops will not handle this.
+				j++
+				k--
+
+			// if the sum is too small, increment `j` to get closer to the target
+			} else if (sum < target) {
+				j++
+
+			// if the sum is too large, decrement `k` to get closer to the target
+			} else { // (sum > target)
+				k--
+			}
+		}
+	}
+
+	return result
 // // Creating a hash set using objects
 // const hashSet = {};
 
--- Learning/Python/codingProblems.py
+++ Learning/Python/codingProblems.py
@@ -13,4 +13,11 @@ topKFrequent("", nums, 2)
 nums = [1,2,3,4]
 def twoSum(nums, target):
 
+    L,R = 0, len(nums) - 1
+
+    for num in nums:
+        while L < R:
+            if nums[L] + nums[R] == target:
+        return true
+
 
