c14a759cb463c6e7af720344a062e487fcbaae55
rishix23 <rishi8318@gmail.com>
2024-01-15 19:59:47 -0500

    solving sliding window in python

--- Learning/Python/codingProblems.py
+++ Learning/Python/codingProblems.py
@@ -1,46 +1,68 @@
-nums = [1, 2, 3, 4, 5, 6]
-target = 4
+## FINISH PROBLEM - BINARY SEARCH
+# def searchInRotatedArray(nums, target):
+#     L = 0
+#     R = len(nums) - 1
+#     M = (L + R) // 2
+
+#     print(nums[L], nums[M], nums[R])
+
+#     if target == nums[M]:
+#         return M
+
+#     if nums[M] >= nums[R] and target <= nums[R]:
+#         print("right side looking")
+#         L = M + 1
+
+#         while L <= R:
+#             M = (L + R) // 2
+#             if target > nums[M]:
+#                 L = M + 1
+#             elif target < nums[M]:
+#                 R = M - 1
+#             else:
+#                 return M
+
+#     elif nums[M] >= nums[L] and target >= nums[L]:
+#         print("left side looking")
+#         R = M - 1
+#         while L <= R:
+#             M = (L + R) // 2
+#             if target > nums[M]:
+#                 L = M + 1
+#             elif target < nums[M]:
+#                 R = M - 1
+#             else:
+#                 return M
+
+#     if target == nums[M]:
+#         return M
+#     else:
+#         return -1
+# nums = [1, 2, 3, 4, 5, 6]
+# target = 4
+
+# print(searchInRotatedArray(nums, target))
 
 
-def searchInRotatedArray(nums, target):
+# CONTAINS NEARBY DUPLICATE SLIDING WINDOW - Time o(n) - Space o(n)
+def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    k = k + 1
     L = 0
-    R = len(nums) - 1
-    M = (L + R) // 2
 
-    print(nums[L], nums[M], nums[R])
+    window = set()
 
-    if target == nums[M]:
-        return M
-
-    if nums[M] >= nums[R] and target <= nums[R]:
-        print("right side looking")
-        L = M + 1
-
-        while L <= R:
-            M = (L + R) // 2
-            if target > nums[M]:
-                L = M + 1
-            elif target < nums[M]:
-                R = M - 1
-            else:
-                return M
-
-    elif nums[M] >= nums[L] and target >= nums[L]:
-        print("left side looking")
-        R = M - 1
-        while L <= R:
-            M = (L + R) // 2
-            if target > nums[M]:
-                L = M + 1
-            elif target < nums[M]:
-                R = M - 1
-            else:
-                return M
-
-    if target == nums[M]:
-        return M
-    else:
-        return -1
+    for R in range(len(nums)):
+        if R - L + 1 > k:
+            window.remove(nums[L])
+            L += 1
+        if nums[R] in window:
+            return True
+        else:
+            window.add(nums[R])
 
 
-print(searchInRotatedArray(nums, target))
+# 19:45
+# 19:55
+nums = [1, 2, 1, 1]
+k = 2
+containsNearbyDuplicate(nums, k)
