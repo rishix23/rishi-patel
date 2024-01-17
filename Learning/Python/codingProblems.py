00c0bdd660efe8a24ff2f43a67c28b83c073f8c3
rishix23 <rishi8318@gmail.com>
2024-01-17 00:03:37 -0500

    solving minimum sub array with sliding window

--- Learning/Python/codingProblems.py
+++ Learning/Python/codingProblems.py
@@ -45,24 +45,49 @@
 
 
 # CONTAINS NEARBY DUPLICATE SLIDING WINDOW - Time o(n) - Space o(n)
-def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-    k = k + 1
-    L = 0
+# def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#     k = k + 1
+#     L = 0
 
-    window = set()
+#     window = set()
 
-    for R in range(len(nums)):
-        if R - L + 1 > k:
-            window.remove(nums[L])
-            L += 1
-        if nums[R] in window:
-            return True
-        else:
-            window.add(nums[R])
+#     for R in range(len(nums)):
+#         if R - L + 1 > k:
+#             window.remove(nums[L])
+#             L += 1
+#         if nums[R] in window:
+#             return True
+#         else:
+#             window.add(nums[R])
 
 
-# 19:45
-# 19:55
-nums = [1, 2, 1, 1]
-k = 2
-containsNearbyDuplicate(nums, k)
+# # 19:45
+# # 19:55
+# nums = [1, 2, 1, 1]
+# k = 2
+# containsNearbyDuplicate(nums, k)
+
+
+# MINIMUM SUB ARRAY - Time - o(n) - Space -
+# def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+#     L = 0
+#     length = float(inf)
+#     totalSum = 0
+
+#     for R in range(len(nums)):
+#         totalSum += nums[R]
+#         while totalSum >= target:
+#             currentLength = R - L + 1
+#             length = min(length, currentLength)
+#             totalSum -= nums[L]
+#             L += 1
+#     if length == float(inf):
+#         return 0
+#     else:
+#         return length
+
+# 23:48
+# 12:01
+# target = 7
+# nums = [2,3,1,2,4,3]
+# minSubArrayLen(target, nums)
